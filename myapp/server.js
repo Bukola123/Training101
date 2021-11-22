d5943453c6304ffceb15b3de929e71241dc74c7b
Bukola 123 <taiwobukola98@gmail.com>
2021-11-22 17:59:24 +0100

    New

---
+++ myapp/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.json);
+app.use(express.static("express"));
+
+app.get/*use*/('/', function(req, res){
+    res.sendFile(path.join(_dirname+'/express/index.html'));
+    //__dirname: it will resolve to your project folder
+})
+
+const server = http.createServer(app);
+const port = 5000;
+server.listen(port);
+
+console.debug('Server listening on port'+ port)
