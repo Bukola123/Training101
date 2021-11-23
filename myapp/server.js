e32ace242c195e5917647fa2ce71c824d0fcd8f3
Bukola 123 <taiwobukola98@gmail.com>
2021-11-23 22:47:39 +0100

    Renaming of express  file to public

--- myapp/server.js
+++ myapp/server.js
@@ -1,13 +1,15 @@
+/*(console.log('The code below is same with the other but different method used'))
 const http = require('http');
 const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.json);
+//app.use(express.json);
 app.use(express.static("express"));
 
+
 app.use('/', function(req, res){
-    res.sendFile(path.join(_dirname+'/express/index.html'));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
     //__dirname: it will resolve to your project folder
 })
 
@@ -15,4 +17,19 @@ const server = http.createServer(app);
 const port = 5000;
 server.listen(port);
 
-console.debug('Server listening on port'+ port)
+console.debug('Server listening on port'+ port)*/
+
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static("express"));
+
+app.get("/",(req, res) => {
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+});
+
+app.listen( 7000, () => {
+    console.log('Example app listening on port 7000!');
+});
