1782f9b8dc22523f00b3a7f5ae4e99564136709a
Bukola 123 <taiwobukola98@gmail.com>
2021-11-24 12:30:58 +0100

    Connecting to database

--- myapp/server.js
+++ myapp/server.js
@@ -21,13 +21,40 @@ console.debug('Server listening on port'+ port)*/
 
 
 const express = require('express');
-const app = express();
 const path = require('path');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
 
-app.use(express.static("express"));
+app.use(express.static("public"));
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb');
+
+const db = mongoose.connection;
+db.on('error',()=> console.log('Error in connecting to Database'));
+db.once('open', () => console.log('Connected to Database'))
+
 
 app.get("/",(req, res) => {
     res.sendFile(path.join(__dirname,"./public/index.html"))
+    var firstName = req.body.firstName;
+    var email = req.body.email;
+
+    var data = {
+        "firstName" : firstName,
+        "email": email
+    }
+    db.collection('users').insertOne(data,(err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record insert successfully");
+    });
+    return res.redirect('./index.js')
 });
 
 app.listen( 7000, () => {
