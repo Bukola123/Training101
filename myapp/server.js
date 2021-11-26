813492a8288d1fb099342a055869aa9b9ce91e7f
Bukola 123 <taiwobukola98@gmail.com>
2021-11-26 10:08:15 +0100

    new

--- myapp/server.js
+++ myapp/server.js
@@ -1,98 +1,3 @@
-/*(console.log('The code below is same with the other but different method used'))
-const http = require('http');
-const express = require('express');
-const path = require('path');
-
-const app = express();
-//app.use(express.json);
-app.use(express.static("express"));
-
-
-app.use('/', function(req, res){
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-    //__dirname: it will resolve to your project folder
-})
-
-const server = http.createServer(app);
-const port = 5000;
-server.listen(port);
-
-console.debug('Server listening on port'+ port)*/
-
-//(Before editting)
-/*const express = require('express');
-const path = require('path');
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const app = express();
-
-app.use(express.static("public"));
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({
-    extended:true
-}))
-
-mongoose.connect('mongodb://localhost:27017/mydb');
-
-const db = mongoose.connection;
-db.on('error',()=> console.log('Error in connecting to Database'));
-db.once('open', () => console.log('Connected to Database'))
-
-app.post("/signup",(req,res) => {
-    var firstName = req.body.firstName;
-    var email = req.body.email;
-
-    var data = {
-        "firstName" : firstName,
-        "email": email
-    };
-    
-});
-
-
-
-app.get("/",(req, res) => {
-    
-    res.sendFile(path.join(__dirname,"./public/index.html"))
-    var firstName = req.body.firstName;
-    var email = req.body.email;
-
-    var data = {
-        "firstName" : firstName,
-        "email": email
-    }
-    
-    db.collection('users').insertOne(data,(err,collection) => {
-        if(err){
-            throw err;
-        }
-        console.log("Record inserted successfully");
-    });
-    return res.redirect('./index.js')
-});
-
-app.get("/",(req,res) => {
-        
-})
-
-app.listen( 7000, () => {
-    console.log('Example app listening on port 7000!');
-});*/
-
-
-//Using the online tutorial
-
-/*var app = express();
-const express = require('express');
-var mongoose = require("mongoose");
-var mongoDB = 'mongodb+srv://Bukola:<12345>@cluster0.j8uhq.mongodb.net/local_library?retryWrites=true&w=majority';
-
-mongoose.connect(mongoDB,{useNewUrlParser: true , useUnifiedTopology: true});
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Mongo connection error:'));
-*/
-
 // Using Youtube video
 var express = require('express');
 var bodyParser = require("body-parser");
@@ -113,13 +18,25 @@ db.on('error',()=> console.log('Error in connecting to Database'));
 db.once('open', () => console.log('Connected to Database'))
 
 app.post("/sign_up",(req,res) => {
+    
     var firstName = req.body.firstName;
     var email = req.body.email;
 
     var data = {
-        "first-name" : firstName,
+        "firstName" : firstName,
         "email": email
     }
+    var Schema = mongoose.Schema;
+
+    var data = new Schema(
+      {
+        firstName: {type: String, required: true, maxLength: 100},
+        email: {type: String, required: true, maxLength: 100, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']},
+             
+    });
+
+    module.exports = mongoose.model('user_data', data);
+
     
     db.collection('users').insertOne(data,(err,collection) => {
         if(err){
